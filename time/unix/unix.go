8eb8221961212bcf9191d77308d14bc6844f1ea8
Kristopher Rahim <kristopherab@gmail.com>
2024-03-30 23:50:13 +0000

    scanning unix

--- time/unix/unix.go
+++ time/unix/unix.go
@@ -11,27 +11,33 @@ type Time int64
 
 func (t Time) Time() time.Time { return time.UnixMilli(int64(t)).UTC() }
 
+func (t Time) Equal(t2 Time) bool { return t == t2 }
+
 func (t *Time) Scan(value any) (err error) {
 	switch v := value.(type) {
 	case int64:
 		*t = Time(v)
 	case string:
-		*t, err = Parse(v)
+		if v[10] == ' ' {
+			*t, err = Parse(time.DateTime, v)
+			return
+		}
+		*t, err = Parse(time.RFC3339, v)
 	default:
 		return fmt.Errorf("unix: unsupported type: %T", v)
 	}
 	return err
 }
 
-func (t Time) Value() (driver.Value, error) { return t, nil }
+func (t Time) Value() (driver.Value, error) { return int64(t), nil }
 
 // Parse parses a formatted string and returns the time value it represents using the RFC3339 format.
-func Parse(s string) (Time, error) {
-	tt, err := time.Parse(time.RFC3339, s)
+func Parse(layout, s string) (Time, error) {
+	tt, err := time.ParseInLocation(layout, s, time.UTC)
 	if err != nil {
 		return 0, err
 	}
-	return Time(tt.UnixMilli()), nil
+	return Time(tt.UTC().UnixMilli()), nil
 }
 
 // FromTime converts a [time.Time] into unix time.
@@ -39,3 +45,10 @@ func FromTime(t time.Time) Time { return Time(t.UTC().UnixMilli()) }
 
 // Now returns the current time using the RFC3339 format.
 func Now() Time { return FromTime(time.Now()) }
+
+func cut(s string, idx int) (before, after string, found bool) {
+	if len(s) > idx && (s[idx] == 'T' || s[idx] == 't') {
+		return s[:idx], s[idx+1:], true
+	}
+	return s, "", false
+}
--- time/unix/unix_test.go
+++ time/unix/unix_test.go
@@ -1,1 +1,47 @@
 package unix_test
+
+import (
+	"context"
+	"testing"
+
+	"go.adoublef.dev/is"
+	"go.adoublef.dev/sdk/database/sql3"
+	. "go.adoublef.dev/sdk/time/unix"
+)
+
+func Test_Time_Scan(t *testing.T) {
+	t.Run("UnixMS", testRoundTrip(func(db *sql3.DB) {
+		is := is.NewRelaxed(t)
+
+		var ut Time
+		err := db.QueryRow(context.TODO(), `select strftime('%s', '2016-10-18') * 1000`).Scan(&ut)
+		is.NoErr(err) // (sql3.DB).QueryRow
+
+		is.Equal(1476748800000, int(ut))
+		t.Log(ut.Time()) // 2016-10-18
+	}))
+
+	t.Run("OK", testRoundTrip(func(db *sql3.DB) {
+		is := is.NewRelaxed(t)
+
+		var ut Time
+		err := db.QueryRow(context.TODO(), `select datetime('2016-10-18T12:34:56Z')`).Scan(&ut)
+		is.NoErr(err) // (sql3.DB).QueryRow
+
+		is.Equal(1476794096000, int(ut))
+		t.Log(ut.Time()) // 2016-10-18
+	}))
+}
+
+func testRoundTrip(f func(*sql3.DB)) func(*testing.T) {
+	return func(t *testing.T) {
+		db, err := sql3.Open(t.TempDir() + "/test.db")
+		if err != nil {
+			t.Fatalf("sql3.Up: %v", err)
+		}
+		t.Cleanup(func() { db.Close() })
+		f(db)
+	}
+}
+
+// 1476745200
