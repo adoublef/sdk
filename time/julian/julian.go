080b0a46475be2fefbc0f645e37e9c9668a67f34
Kristopher Rahim <kristopherab@gmail.com>
2024-03-30 23:50:24 +0000

    scanning julian

--- time/julian/julian.go
+++ time/julian/julian.go
@@ -3,6 +3,7 @@ package julian
 import (
 	"database/sql/driver"
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -14,6 +15,33 @@ const (
 // Time
 type Time float64
 
+func (t Time) String() string {
+	return t.Time().Format(time.RFC3339)
+}
+
+func (t Time) Equal(t2 Time) bool {
+	epsilon := math.Nextafter(1, 2) - 1
+	// https://floating-point-gui.de/errors/comparison/#look-out-for-edge-cases
+	a := math.Abs(float64(t))
+	b := math.Abs(float64(t2))
+	diff := math.Abs(float64(t - t2))
+	if t == t2 {
+		return true
+	} else if t == 0 || t2 == 0 || (a+b < math.SmallestNonzeroFloat64) {
+		return diff < epsilon*math.SmallestNonzeroFloat64
+	}
+	return diff/math.Min(a+b, math.MaxFloat64) < epsilon
+}
+
+func (t *Time) UnmarshalText(p []byte) (err error) {
+	*t, err = Parse(time.RFC3339, string(p))
+	return
+}
+
+func (t Time) MarshalText() ([]byte, error) {
+	return []byte(t.String()), nil
+}
+
 // Time returns t as [time.Time]
 func (t Time) Time() time.Time {
 	return time.Unix(int64((t-UnixEpochJulianDay)*DaySeconds), 0).UTC()
@@ -21,15 +49,23 @@ func (t Time) Time() time.Time {
 
 func (t *Time) Scan(value any) (err error) {
 	switch v := value.(type) {
-	case int64:
+	case float64:
 		*t = Time(v)
+	case string:
+		// civil time should also work
+		switch len(v) {
+		case 10: // yyyy-mm-dd
+			*t, err = Parse(time.DateOnly, v)
+		default:
+			*t, err = Parse(time.RFC3339, v)
+		}
 	default:
-		return fmt.Errorf("unix: unsupported type: %T", v)
+		return fmt.Errorf("julian: unsupported type: %T", v)
 	}
 	return err
 }
 
-func (t Time) Value() (driver.Value, error) { return t, nil }
+func (t Time) Value() (driver.Value, error) { return float64(t), nil }
 
 // FromTime converts a [time.Time] into a Julian day.
 func FromTime(t time.Time) Time {
@@ -38,3 +74,12 @@ func FromTime(t time.Time) Time {
 
 // Now returns the current time. Location is set to UTC.
 func Now() Time { return FromTime(time.Now()) }
+
+// Parse
+func Parse(layout, s string) (Time, error) {
+	t, err := time.Parse(layout, s)
+	if err != nil {
+		return 0, err
+	}
+	return FromTime(t), nil
+}
